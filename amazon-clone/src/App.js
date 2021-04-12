65d92dae73f8dc5e3b26f25f0bdf06d1d12a13ed
Andy <a4heng@ucsd.edu>
2021-04-11 17:25:56 -0700

    introduced react router

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import Header from './Header'
 import Home from './Home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Checkout from './Checkout';
 
 function App() {
   return (
     // Bem convention
-    <div className="app">
-      {/* <h1>Hello World!</h1> */}
-      {/** Header */}
-      <Header />
-      {/** Home */}
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/checkout">
+            <h1>Check out</h1>
+            <Checkout />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -3,7 +3,18 @@ import './Product.css'
 function Product() {
   return (
     <div className="product">
-      
+      <div className="product__info">
+        <p>The Lean Startup</p>
+        <p className='product__price'>
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+        <div className="product__rating">
+          <p>⭐</p>
+        </div>
+      </div>
+      <img src="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"></img>
+      <button>Add to Cart</button>
     </div>
   )
 }
