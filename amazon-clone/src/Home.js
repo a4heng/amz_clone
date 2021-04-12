8a2be893953fb4498dcbcacbdd232a0eaa90444a
Andy <a4heng@ucsd.edu>
2021-04-11 17:26:36 -0700

    rendered out cards

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -9,20 +9,52 @@ function Home() {
 
         <div className="home__row">
           {/**Produc Component */}
-          <Product />
+          <Product 
+          title="The Lean startup" 
+          price="29.99" 
+          image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+          rating={5}
+          />
+          <Product 
+          title="T-fal Deep Fryer with Basket, Stainless Steel, Easy to Clean Deep Fryer, Oil Filtration, 2.6-Pound, Silver, Model FR8000"
+          price="114.29"
+          image="https://m.media-amazon.com/images/I/713j2j371bL._AC_UY436_FMwebp_QL65_.jpg"
+          rating={5}
+          />
           {/**Produc Component */}
         </div>
 
         <div className="home__row">
           {/**Produc Component */}
-          {/**Produc Component */}
-          {/**Produc Component */}
+          <Product 
+          title="New Apple iPad Air (10.9-inch, Wi-Fi, 64GB) - Sky Blue (Latest Model, 4th Generation)"
+          price="559.98"
+          image="https://m.media-amazon.com/images/I/71FePRgkiZL._AC_UY436_FMwebp_QL65_.jpg"
+          rating={4}
+          />
+          <Product 
+            title="All-new Echo (4th Gen) | With premium sound, smart home hub, and Alexa | Charcoal"
+            price="99.99"
+            image="https://m.media-amazon.com/images/I/71JB6hM6Z6L._AC_UL640_FMwebp_QL65_.jpg"
+            rating={5}
+          />
+          <Product 
+            title="Cubiker Modern L-Shaped Desk Computer Corner Desk, PC Laptop Writing Study Desk for Home Office Wood & Metal, Rustic Brown"
+            price="79.99"
+            image="https://m.media-amazon.com/images/I/711a4xBzp+L._AC_UL640_FMwebp_QL65_.jpg"
+            rating={5}
+          />
         </div>
 
         <div className="home__row">
-          {/**Produc Component */}
+          <Product 
+            title='Sceptre Curved 27" 75Hz LED Monitor HDMI VGA Build-In Speakers, EDGE-LESS Metal Black 2019 (C275W-1920RN)'
+            price="179.99"
+            image='https://m.media-amazon.com/images/I/914W+OtJQ-L._AC_UL640_FMwebp_QL65_.jpg'
+            rating={4}
+          />
         </div>
-        
+
       </div>
     </div>
   )
